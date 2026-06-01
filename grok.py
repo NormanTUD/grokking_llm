ee4ede7263d4e7c878468e47570b91e4ebb2ab50
n <norman.koch@tu-dresden.de>
2026-06-01 07:37:02 +0200

    fix

--- grok.py
+++ grok.py
@@ -2516,5 +2516,5 @@ if __name__ == "__main__":
     demo = build_gui()
     try:
         demo.launch(share=False, server_name="0.0.0.0", server_port=7860)
-    except OSError:
+    except OSError as e:
         print(e)
